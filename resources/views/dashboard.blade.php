e86e77a053caf892f36f951f70a34b8b1104838e
CMTZ10 <79880102+CMTZ10@users.noreply.github.com>
2021-03-04 15:41:40 -0700

    version 1

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,27 @@
+@extends('adminlte::page')
+
+@section('title', 'Inicio')
+
+@section('content_header')
+    <h1>UISCLOUD</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-header">
+            <h1 class="card-title">Bienvenidos a UISCLOUD</h1>
+        </div>
+
+        <div class="card-body">
+            <p>Texto de prueba de card body</p>
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> //console.log('Hi!'); </script>
+@stop
